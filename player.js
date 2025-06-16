6af3f3c48423d502e7198be7118cc23ad63c8f01
PMisar <147728448+PMisar@users.noreply.github.com>
2025-06-16 06:52:44 +0200

    new commit

--- player.js
+++ player.js
@@ -1,137 +1,4 @@
-// // player.js
-// class Player {
-//   constructor(gameScreen, left, top, width, height, imgSrc) {
-//     this.left = left;
-//     this.top = top;
-//     this.width = width;
-//     this.height = height;
-//     this.directionX = 0;
-//     this.directionY = 0;
-//     this.gameScreen = gameScreen;
-//     this.imageSrc = imgSrc; // player facing left image
-//     this.element = document.createElement("img");
-//     this.element.src = imgSrc;
-//     this.element.style.position = "absolute";
-//     this.element.style.width = `${width}px`;
-//     this.element.style.height = `${height}px`;
-//     this.element.style.left = `${left}px`;
-//     this.element.style.top = `${top}px`;
-//     this.gameScreen.appendChild(this.element);
-
-//     this.velocityY = 0;
-//     this.gravity = 0.4;
-//     this.jumpStrength = -10;
-//     this.isJumping = false;
-//   }
-
-//   handlePlatformCollision(platform) {
-//     this.top = platform.top - this.height;
-//     this.velocityY = 0;
-//     this.isJumping = false;
-//   }
-
-//   move(deltaTime) {
-//     this.velocityY += this.gravity;
-
-//     if (this.isJumping) {
-//       if (this.directionX < 0) {
-//         this.imageSrc = "./images/player-left-jump.png"; // jumping left image source
-//       } else if (this.directionX > 0) {
-//         this.imageSrc = "./images/player-right-jump.png"; // jumping right image source
-//       }
-//     } else {
-//       if (this.directionX < 0) {
-//         this.imageSrc = "./images/player-left.png"; // left-facing image source
-//       } else if (this.directionX > 0) {
-//         this.imageSrc = "./images/player-right.png"; // right-facing image source
-//       }
-//     }
-
-//     this.top += this.velocityY;
-
-//     if (this.top > this.gameScreen.offsetHeight - this.height) {
-//       this.top = this.gameScreen.offsetHeight - this.height;
-//       this.velocityY = 0;
-//       this.isJumping = false;
-//     }
-
-//     const expectedFrameTime = 1 / 60;
-//     const frameMultiplier = deltaTime ? (deltaTime / expectedFrameTime) : 1;
-//     this.left += this.directionX * frameMultiplier;
-//     this.top += this.directionY;
-
-//     this.left = Math.max(20, Math.min(this.gameScreen.offsetWidth - this.width - 20, this.left));
-//     // this.top = Math.min(this.gameScreen.offsetHeight - this.height - 20, this.top);
-
-//     this.updatePosition();
-//   }
-
-//   updatePosition() {
-//     this.element.src = this.imageSrc;
-//     this.element.style.left = `${this.left}px`;
-//     this.element.style.top = `${this.top}px`;
-//   }
-
-//   isCollidingWith(platform) {
-//     let playerLeft = this.left;
-//     let playerRight = this.left + this.width;
-//     let playerTop = this.top;
-//     let playerBottom = this.top + this.height;
-
-//     const platformLeft = platform.left;
-//     const platformRight = platform.left + platform.width;
-//     const platformTop = platform.top;
-//     const platformBottom = platform.top + platform.height;
-
-//     if (
-//       playerLeft < platformRight &&
-//       playerRight > platformLeft &&
-//       playerTop < platformBottom &&
-//       playerBottom > platformTop
-//     ) {
-//       const overlapLeft = playerRight - platformLeft;
-//       const overlapRight = platformRight - playerLeft;
-//       const overlapTop = playerBottom - platformTop;
-//       const overlapBottom = platformBottom - playerTop;
-
-//       const minOverlap = Math.min(
-//         overlapLeft,
-//         overlapRight,
-//         overlapTop,
-//         overlapBottom
-//       );
-
-//       if (minOverlap === overlapLeft) {
-//         this.left -= overlapLeft;
-//         if (this.velocityY > 0) {
-//           this.velocityY = 0;
-//           this.isJumping = false;
-//         }
-//       } else if (minOverlap === overlapRight) {
-//         this.left += overlapRight;
-//         if (this.velocityY > 0) {
-//           this.velocityY = 0;
-//           this.isJumping = false;
-//         }
-//       } else if (minOverlap === overlapTop) {
-//         this.top -= overlapTop;
-//         if (this.velocityY > 0) {
-//           this.velocityY = 0;
-//           this.isJumping = false;
-//         }
-//       } else if (minOverlap === overlapBottom) {
-//         this.top += overlapBottom;
-//         if (this.velocityY < 0) {
-//           this.velocityY = 0;
-//         }
-//       }
-//       return true;
-//     }
-//     return false;
-//   }
-// } 
-
-// player.js - Fixed version
+// player.js 
 class Player {
   constructor(gameScreen, left, top, width, height, imgSrc) {
     this.left = left;
@@ -155,7 +22,7 @@ class Player {
     this.gravity = 0.4;
     this.jumpStrength = -10;
     this.isJumping = false;
-    
+
     // Fixed: Use pixels per second instead of pixels per frame
     this.moveSpeed = 240; // pixels per second (equivalent to 4 pixels at 60fps)
   }
@@ -168,8 +35,8 @@ class Player {
 
   move(deltaTime) {
     // Fixed: Use actual deltaTime for all movement calculations
-    const dt = deltaTime || (1/60); // Fallback to 60fps if deltaTime is 0
-    
+    const dt = deltaTime || (1 / 60); // Fallback to 60fps if deltaTime is 0
+
     this.velocityY += this.gravity;
 
     if (this.isJumping) {
